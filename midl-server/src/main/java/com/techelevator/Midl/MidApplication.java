fe8652c5f9108f714c39f5f6e7aeb3d2c14d0bae
Ding Jiang <dingj106@gmail.com>
2024-08-01 11:42:31 -0400

    Change tenmo to midl

--- midl-server/src/main/java/com/techelevator/Midl/MidApplication.java
+++ midl-server/src/main/java/com/techelevator/Midl/MidApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TenmoApplication {
+public class MidApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TenmoApplication.class, args);
